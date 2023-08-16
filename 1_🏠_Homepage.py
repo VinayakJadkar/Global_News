65909d14225e5751f99ecca4308ff8a75c8ff78d
Vinayak Jadkar <71877945+VinayakJadkar@users.noreply.github.com>
2023-08-17 02:21:54 +0530

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Define your NewsAPI.org API key
-api_key = "pub_27492aab2f2ae3b24de7a73a5c1d952d82ace"
-
-
 
 # Set page configuration to wide layout
 st.set_page_config(page_title="Homepage",page_icon="🏠", layout="wide")
